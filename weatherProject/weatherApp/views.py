bce21c090b0f68f03d62b6d818286227e5db7947
Malsor <malsorkrasniqi60@gmail.com>
2023-07-22 15:23:44 +0200

    Request fix

--- weatherProject/weatherApp/views.py
+++ weatherProject/weatherApp/views.py
@@ -1,42 +1,34 @@
 import urllib.request
 import json
-import pycountry
 from django.shortcuts import render
 
 
 def index(request):
-    if request.method == 'POST':    
+    if request.method == 'POST':
+        
+         
         try:
-            city = request.POST['city'] 
-            
-            source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+
-                                         city.replace(" ", "%20")+'&units=metric&appid=9235bd39b8748d9dc00cae108684f34b')
-            
+            city = request.POST['city']
+            source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                            city.replace(" ", "%20") + '&units=metric&appid=9235bd39b8748d9dc00cae108684f34b')
+
             list_of_data = json.loads(source.read())
 
-            data = { 
+            data = {
+                "city_name": str(list_of_data['name']),
                 "country_code": str(list_of_data['sys']['country']),
-                "wind": str(list_of_data['wind']['speed']),
+                "wind": str(list_of_data['wind']['speed']), 
                 "temp": str(list_of_data['main']['temp']) + ' °C',
-                "pressure": str(list_of_data['main']['pressure']),  
+                "pressure": str(list_of_data['main']['pressure']),
                 "humidity": str(list_of_data['main']['humidity']),
                 'main': str(list_of_data['weather'][0]['main']),
                 'description': str(list_of_data['weather'][0]['description']),
                 'icon': list_of_data['weather'][0]['icon'],
-                
+
             }
-
-            country_code = str(list_of_data['sys']['country'])
-
-            country_name = pycountry.countries.get(alpha_2=country_code).name
-
-            data["country_name"] = country_name
-
             print(data)
             return render(request, "main/index.html", data)
         except:
             data = {"error_message": "error_message"}
 
     return render(request, "main/index.html")
-
-
